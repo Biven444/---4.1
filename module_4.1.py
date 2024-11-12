77ff7530bb39a9c4f6df454a6a4b2724b1724569
Biven444 <biven2010@gmail.com>
2024-11-12 14:59:31 +0300

    Add files via upload

---
+++ module_4.1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as fd
+from true_math import divide as td
+
+result_1 = fd (69,3)
+result_2 = fd (3,0)
+result_3 = td(49,7)
+result_4 = td(15,0)
+print(result_1)
+print(result_2)
+print(result_3)
+print(result_4)
